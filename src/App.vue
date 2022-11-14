ec9cf3e90c18066bccc7370947d49eaf5338765e
nirav <nirav.gaglani@improwised.com>
2022-11-14 11:57:39 +0530

    AddressBook Code CRUD implemented

--- src/App.vue
+++ src/App.vue
@@ -1,124 +1,3 @@
-<!-- <script setup>
-
-import { ref, reactive } from 'vue';
-import list from './components/list.vue';
-import add from './components/add.vue';
-
-let addresses = ref([{name: "Nirav", address: "Bansari Society", contactNo: "6353492281", email: "niravgaglani6268@gmail.com", country: "India", state: "Gujarat"}, {name: "Suryakant", address: "Kuvadwa Road", contactNo: "9377151135", email: "suri@gmail.com", country: "India", state: "Gujarat"}, {name: "Vishwajeet", address: "Aalap Heritage", contactNo: "9913429071", email: "vishu@gmail.com", country: "India", state: "Gujarat"}]);
-
-// let addresses = ref([{name: "Nirav", address: "Bansari Society", contactNo: "6353492281", email: "niravgaglani6268@gmail.com", country: "India", state: "Gujarat"}, {name: "Suryakant", address: "Kuvadwa Road", contactNo: "9377151135", email: "suri@gmail.com", country: "India", state: "Gujarat"}, {name: "Vishwajeet", address: "Aalap Heritage", contactNo: "9913429071", email: "vishu@gmail.com", country: "India", state: "Gujarat"}]);
-
-let fields = ['name', 'address', 'contactNo', 'email', 'country', 'state'];
-
-let addObj = reactive({
-    name: '',
-    address: '',
-    contactNo: '',
-    email: '',
-    country: '',
-    state: ''
-});
-
-function addAddress(newObj){
-  console.log("addAdd called");
-  addresses.value.push(newObj);
-}
-
-function removeAddress(obj){
-  addresses.value = addresses.value.filter(item=>item!==obj);
-  return addresses.value
-}
-
-function updateAddress(obj){
-  addresses.value.forEach(function(item, index){
-    if (item===obj){
-      console.log(item.address);
-    }
-  })
-}
-
-</script>
-
-<template>
-  <div>
-    <add :data="addresses" :dataFields="fields" @response="newObj=>addAddress(newObj)" />
-  </div>
-
-  <div>
-    <list :data="addresses" :dataFields="fields" @deleteResponse="obj=>removeAddress(obj)" @updateResponse="obj=>updateAddress(obj)"/>
-  </div>
-</template> -->
-<script setup>
-import { ref, reactive } from "vue";
-import list from "./components/list.vue";
-import add from "./components/add.vue";
-
-let addresses = ref([
-  {
-    name: "Nirav",
-    address: "Bansari Society",
-    contactNo: "6353492281",
-    email: "niravgaglani6268@gmail.com",
-    country: "India",
-    state: "Gujarat",
-  },
-  {
-    name: "Suryakant",
-    address: "Kuvadwa Road",
-    contactNo: "9377151135",
-    email: "suri@gmail.com",
-    country: "India",
-    state: "Gujarat",
-  },
-  {
-    name: "Vishwajeet",
-    address: "Aalap Heritage",
-    contactNo: "9913429071",
-    email: "vishu@gmail.com",
-    country: "India",
-    state: "Gujarat",
-  },
-]);
-
-let fields = ["name", "address", "contactNo", "email", "country", "state"];
-
-let defaultObj = ref({
-  name: "",
-  address: "",
-  contactNo: "",
-  email: "",
-  country: "",
-  state: "",
-});
-
-function addAddress(newObj) {
-  let index = addresses.value.indexOf(newObj);
-  if (index !== -1) {
-    alert("Update your old address?");
-    addresses[index].name = newObj.name;
-    addresses[index].address = newObj.address;
-    addresses[index].contactNo = newObj.contactNo;
-    addresses[index].email = newObj.email;
-    addresses[index].country = newObj.country;
-    addresses[index].state = newObj.state;
-  } else {
-    addresses.value.push(newObj);
-    alert("new object added");
-  }
-}
-
-function removeAddress(obj) {
-  console.log(obj);
-  addresses.value = addresses.value.filter((item) => item !== obj);
-  return addresses.value;
-}
-
-function updateAddress(obj) {
-  defaultObj.value = obj;
-  console.log("defaultObj--->", defaultObj.value);
-}
-</script>
-
 <template>
   <div>
     <add
@@ -136,3 +15,75 @@ function updateAddress(obj) {
     />
   </div>
 </template>
+
+  <script setup>
+  import { ref, reactive } from "vue";
+  import list from "./components/list.vue";
+  import add from "./components/add.vue";
+  
+  let addresses = ref([
+    {
+      name: "Nirav",
+      address: "Bansari Society",
+      contactNo: "6353492281",
+      email: "niravgaglani6268@gmail.com",
+      country: "India",
+      state: "Gujarat",
+    },
+    {
+      name: "Suryakant",
+      address: "Kuvadwa Road",
+      contactNo: "9377151135",
+      email: "suri@gmail.com",
+      country: "India",
+      state: "Gujarat",
+    },
+    {
+      name: "Vishwajeet",
+      address: "Aalap Heritage",
+      contactNo: "9913429071",
+      email: "vishu@gmail.com",
+      country: "India",
+      state: "Gujarat",
+    },
+  ]);
+  
+  let fields = ["name", "address", "contactNo", "email", "country", "state"];
+  
+  let defaultObj = ref({
+    name: "",
+    address: "",
+    contactNo: "",
+    email: "",
+    country: "",
+    state: "",
+  });
+  
+  function addAddress(newObj) {
+    let index = addresses.value.indexOf(newObj);
+    if (index !== -1) {
+      alert("Update your old address?");
+      addresses[index].name = newObj.name;
+      addresses[index].address = newObj.address;
+      addresses[index].contactNo = newObj.contactNo;
+      addresses[index].email = newObj.email;
+      addresses[index].country = newObj.country;
+      addresses[index].state = newObj.state;
+    } else {
+      addresses.value.push(newObj);
+      alert("new object added");
+    }
+  }
+  
+  function removeAddress(obj) {
+    console.log(obj);
+    addresses.value = addresses.value.filter((item) => item !== obj);
+    return addresses.value;
+  }
+  
+  function updateAddress(obj) {
+    defaultObj.value = obj;
+    console.log("defaultObj--->", defaultObj.value);
+  }
+  </script>
+  
--- src/components/add.vue
+++ src/components/add.vue
@@ -1,80 +1,3 @@
-<!-- <script setup>
-import { reactive, ref } from "@vue/reactivity";
-
-
-let newObj = reactive({
-    name: '',
-    address: '',
-    contactNo: '',
-    email: '',
-    country: '',
-    state: ''
-
-});
-
-const props = defineProps({
-    data: Array,
-    dataFields: Array,
-});
-
-const emit = defineEmits(['response']);
-function sendEmits(){
-    emit('response', newObj);
-}
-
-</script>
-
-<template>
-    <form action="">
-        <div>
-        <label for="">Name :</label>
-        <input type="text" v-model="newObj.name" placeholder="name" id="name">
-        </div>
-        
-        <div>
-        <label for="">Address :</label>
-        <input type="text" v-model="newObj.address" placeholder="address" id="address">
-        </div>
-        
-        <div>
-        <label for="">Contact No. :</label>
-        <input type="text" v-model="newObj.contactNo" placeholder="contactNo" id="contactNo">
-        </div>
-        
-        <div>
-        <label for="">Email ID :</label>
-        <input type="email" v-model="newObj.email" placeholder="email" id="email">
-        </div>
-        
-        <div>
-        <label for="">Country :</label>
-        <input type="text" v-model="newObj.country" placeholder="country" id="country">
-        </div>
-        
-        <div>
-        <label for="">State :</label>
-        <input type="text" v-model="newObj.state" placeholder="state" id="state">
-        </div>
-        <button type="submit" @click="sendEmits" class="btn btn-primary">Submit</button>
-    </form>
-</template> -->
-
-<script setup>
-const props = defineProps({
-  defaultValues: Object,
-});
-
-const emit = defineEmits(["response"]);
-
-function sendEmits() {
-  emit("response", props.defaultValues);
-}
-
-function consoleNewObj() {
-  console.log("007", props.defaultValues);
-}
-</script>
-
 <template>
   <form action="">
     <div>
@@ -124,16 +47,16 @@ function consoleNewObj() {
         v-model="defaultValues.country"
         placeholder="country"
         id="country"
-      />
-    </div>
+        />
+      </div>
 
     <div>
       <label for="state">State :</label>
       <input
-        type="text"
-        v-model="defaultValues.state"
-        placeholder="state"
-        id="state"
+      type="text"
+      v-model="defaultValues.state"
+      placeholder="state"
+      id="state"
       />
     </div>
     <button type="button" @click="sendEmits" class="btn btn-warning">
@@ -141,3 +64,19 @@ function consoleNewObj() {
     </button>
   </form>
 </template>
+
+<script setup>
+const props = defineProps({
+  defaultValues: Object,
+});
+
+const emit = defineEmits(["response"]);
+
+function sendEmits() {
+  emit("response", props.defaultValues);
+}
+
+function consoleNewObj() {
+  console.log("007", props.defaultValues);
+}
+</script>
