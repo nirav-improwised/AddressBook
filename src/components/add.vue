9a43f83335610a618b6625246f6298483e1e91ed
nirav <nirav.gaglani@improwised.com>
2022-11-14 11:52:04 +0530

    addressBook code added

--- src/components/add.vue
+++ src/components/add.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import { reactive, ref } from "@vue/reactivity";
 
 
@@ -57,4 +57,87 @@ function sendEmits(){
         </div>
         <button type="submit" @click="sendEmits" class="btn btn-primary">Submit</button>
     </form>
+</template> -->
+
+<script setup>
+const props = defineProps({
+  defaultValues: Object,
+});
+
+const emit = defineEmits(["response"]);
+
+function sendEmits() {
+  emit("response", props.defaultValues);
+}
+
+function consoleNewObj() {
+  console.log("007", props.defaultValues);
+}
+</script>
+
+<template>
+  <form action="">
+    <div>
+      <label for="name">Name :</label>
+      <input
+        type="text"
+        v-model="defaultValues.name"
+        placeholder="name"
+        id="name"
+      />
+    </div>
+
+    <div>
+      <label for="address">Address :</label>
+      <input
+        type="text"
+        v-model="defaultValues.address"
+        placeholder="address"
+        id="address"
+      />
+    </div>
+
+    <div>
+      <label for="contactNo">Contact No. :</label>
+      <input
+        type="text"
+        v-model="defaultValues.contactNo"
+        placeholder="contactNo"
+        id="contactNo"
+      />
+    </div>
+
+    <div>
+      <label for="email">Email ID :</label>
+      <input
+        type="email"
+        v-model="defaultValues.email"
+        placeholder="email"
+        id="email"
+      />
+    </div>
+
+    <div>
+      <label for="country">Country :</label>
+      <input
+        type="text"
+        v-model="defaultValues.country"
+        placeholder="country"
+        id="country"
+      />
+    </div>
+
+    <div>
+      <label for="state">State :</label>
+      <input
+        type="text"
+        v-model="defaultValues.state"
+        placeholder="state"
+        id="state"
+      />
+    </div>
+    <button type="button" @click="sendEmits" class="btn btn-warning">
+      Add address
+    </button>
+  </form>
 </template>
--- src/components/list.vue
+++ src/components/list.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import { reactive } from "@vue/reactivity";
 
 const props = defineProps({
@@ -83,4 +83,94 @@ th.active {
 th.active .arrow {
   opacity: 1;
 }
-</style>
+</style> -->
+
+<template>
+    <table>
+      <thead>
+        <tr>
+          <th v-for="(field, index) in props.dataFields" :key="index">
+            {{ field }}
+          </th>
+          <th>delete</th>
+          <th>update</th>
+        </tr>
+      </thead>
+  
+      <tbody v-if="props.data.length > 0">
+        <tr v-for="(obj, index) in data" :key="index">
+          <td v-for="(objField, i) in dataFields" :key="i">
+            {{ obj[objField] }}
+          </td>
+          <td>
+            <button class="btn btn-outline-success" @click="emitDeleteObj(obj)">
+              Delete
+            </button>
+          </td>
+          <td>
+            <button class="btn btn-outline-success" @click="emitUpdateObj(obj)">
+              Update
+            </button>
+          </td>
+        </tr>
+      </tbody>
+  
+      <tbody v-else>
+        <tr>
+          <td colspan="6">No data found</td>
+        </tr>
+      </tbody>
+    </table>
+  </template>
+  
+  <style>
+  table {
+    border: 2px solid #42b983;
+    border-radius: 3px;
+    background-color: #fff;
+  }
+  
+  th {
+    background-color: #42b983;
+    color: rgba(255, 255, 255, 0.66);
+    cursor: pointer;
+    user-select: none;
+  }
+  
+  td {
+    background-color: #f9f9f9;
+  }
+  
+  th,
+  td {
+    min-width: 120px;
+    padding: 10px 20px;
+  }
+  
+  th.active {
+    color: #fff;
+  }
+  
+  th.active .arrow {
+    opacity: 1;
+  }
+  </style>
+  
+  <script setup>
+  import { reactive } from "vue";
+  
+  const props = defineProps({
+    data: Array,
+    dataFields: Array,
+  });
+  
+  const emit = defineEmits(["deleteResponse", "updateResponse"]);
+  
+  function emitDeleteObj(obj) {
+    emit("deleteResponse", obj);
+  }
+  function emitUpdateObj(obj) {
+    emit("updateResponse", obj);
+  }
+  </script>
+  
